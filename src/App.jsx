26ef59ebc15b1ba8829f0ef335e20062409e5a19
eringorokva-sketch <eringorokva@gmail.com>
2025-10-29 01:38:32 +0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,5 @@ import React from "react";
 import PrescriptionApp from "./components/PrescriptionApp";
 
 export default function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 p-6">
-      <PrescriptionApp />
-    </div>
-  );
+  return <PrescriptionApp />;
 }
